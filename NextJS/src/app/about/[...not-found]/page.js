c7d65b4cc961093191d97ffe3bcfc441b7af3058
Gopal Pokhrel <gopalpokhrel101@gmail.com>
2024-01-07 21:16:25 +0545

    404 Not found

---
+++ NextJS/src/app/about/[...not-found]/page.js
@@ -0,0 +1,7 @@
+export default function Notfound(){
+    return(
+        <> 
+        <h1>Page is not found</h1>
+         </>
+    )
+}
---
+++ NextJS/src/app/not-found.js
@@ -0,0 +1,9 @@
+export default function Notfound(){
+    return(
+        <>
+        <h1>This page is not found</h1>  
+        </>
+    )
+}
+
+//global page
