72a012753f5c305a2f18724e1c99838ebede72bf
Gopal Pokhrel <gopalpokhrel101@gmail.com>
2024-01-08 10:06:07 +0545

    Conditional Style

--- css-in-next/src/app/page.js
+++ css-in-next/src/app/page.js
@@ -1,13 +1,26 @@
+'use client'
+import { useState } from 'react'
 import file from './style.module.css'
-import outside from '@/outsidefile/outside.module.css'
+
 
 
 export default function Home() {
 
+  const [color, setColor] = useState('red');
+  const {red}= file;
+
   return (
     <>
-         <h1 className={file.main}>Modular css</h1>
-         <h2 className={outside.main}>Outside file</h2>
+     <h1 className={color=='red'?file.red:file.green}>Heading tag 1</h1>
+     <h2 style={{backgroundColor: color=='red'?"red":"green"}}>Heading tag 2</h2>
+     <h3 id={file.black}>Heading tag 3</h3>
+
+     <button onClick={()=> setColor('green')}>Change Color</button>
+
+    <h4 className={red}>Last tag</h4>
+    <h4 className={red}>Last tag</h4>
+    <h4 className={red}>Last tag</h4>
+    <h4 className={red}>Last tag</h4>
     </>
     
   )
